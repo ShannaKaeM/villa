d7372109d921734a325fd4b8b4f2c695953dffda
miAgency <accounts@middinc.com>
2025-05-21 18:24:28 -0400

    Initial setup of Blocksy child theme with Carbon Fields and CPT registration

---
+++ app/public/wp-content/themes/blocksy-child/functions.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Blocksy Child Theme Functions
+ */
+
+// Load Composer autoloader
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+// Enqueue stylesheets in the correct order
+function blocksy_child_enqueue_styles() {
+    // 1. First load parent theme stylesheet
+    wp_enqueue_style(
+        'blocksy-parent-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        wp_get_theme('blocksy')->get('Version')
+    );
+    
+    // 2. Then load child theme stylesheet for any additional customizations
+    wp_enqueue_style(
+        'blocksy-child-style',
+        get_stylesheet_uri(),
+        array('blocksy-parent-style'),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'blocksy_child_enqueue_styles');
+
+// Carbon Fields setup - this MUST come first before any other includes
+require_once get_stylesheet_directory() . '/inc/carbon-fields-setup.php';
+
+// Include CPT and taxonomy registration
+require_once get_stylesheet_directory() . '/inc/mi-cpt-registration.php';
+
+/**
+ * Add your custom functions below this line
+ */
+
+// Block registration will be added here
